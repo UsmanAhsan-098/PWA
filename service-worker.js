a648b3ec88894c89833ea1e058f798019f6020c8
UsmanAhsan-098 <78911647+UsmanAhsan-098@users.noreply.github.com>
2021-06-03 12:51:51 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -23,3 +23,27 @@ return cache.addAll(cacheFiles);
 }) 
 );
 });
+
+
+/* self.addEventListener('fetch', function (e) {
+    e.respondWith(
+    caches.match(e.request).then(function (r) {
+    console.log('[Service Worker] Fetching resource: ' + e.request.url);
+        return r 
+        }) 
+        );
+     }); */
+
+
+     self.addEventListener('fetch', function (e) {
+      e.respondWith(
+    caches.match(e.request).then(function (r) {
+         return r || fetch(e.request).then(function (response) {
+             return caches.open(cacheName).then(function (cache) { 
+             cache.put(e.request, response.clone());
+             return response; 
+             });
+             });  
+              })
+                );
+            });
